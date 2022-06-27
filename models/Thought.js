5ceb7e1a6595600db945a4aeac01dad003e2c8b3
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-06-27 14:48:52 +1000

    populated thought model

---
+++ models/Thought.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema({
+	thoughtText: {
+		type: String,
+		required: true,
+		minLength: 1,
+		maxLength: 280
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+		get: (v) => {
+			return v.toLocaleDateString(undefined, {
+				weekday: 'long',
+				year: 'numeric',
+				month: 'long',
+				day: 'numeric'
+			});
+		}
+	},
+	username: {
+		type: String,
+		required: true,
+	},
+	reactions: [reactionSchema]
+},
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true
+		},
+		id: false
+	});
+
+// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+thoughtSchema.virtual('reactionCount').get(function () {
+	return this.reactions.length;
+});
+const Thought = model('thought', thoughtSchema);
+module.exports = Thought;
